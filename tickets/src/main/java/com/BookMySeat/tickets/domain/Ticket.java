02f02374f714eb57d2c6f2bc51fac5b44152ad5c
MiteshBari23 <barimitesh23@gmail.com>
2025-12-09 13:41:13 +0530

    created ticketvalidation class and added validation refrence to ticket class

---
+++ tickets/src/main/java/com/BookMySeat/tickets/domain/Ticket.java
@@ -0,0 +1,56 @@
+package com.BookMySeat.tickets.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.apache.catalina.LifecycleState;
+import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tickets")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Ticket {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "status" , nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TicketStatusEnum status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_type_id")
+    private TicketType ticketType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "purchaser_id")
+    private User purchaser;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL)
+    private List<TicketValidation> validations = new ArrayList<>();
+
+    //TODO QR CODE
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+
+}
