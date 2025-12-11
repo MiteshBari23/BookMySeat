1e3ec9c3d9574eb65a160f8304b2f2acdc285d60
MiteshBari23 <barimitesh23@gmail.com>
2025-12-11 12:33:06 +0530

    created basic spring security configuration file

---
+++ tickets/src/main/java/com/BookMySeat/tickets/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.BookMySeat.tickets.repository;
+
+import com.BookMySeat.tickets.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
